bd6538f8b5bebfde101be6dca398ed3b686033ff
luisgeek617 <50648891+luisgeek617@users.noreply.github.com>
2025-03-05 08:38:37 +0800

    test all the provided cases
    tested cases can be found in the input folder, the corresponding ground truth are in the output folder

--- test_all_cases.py
+++ test_all_cases.py
@@ -5,7 +5,6 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-
 #from tensorflow.keras.models import Sequential, load_model
 #from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 #from tensorflow.keras.utils import to_categorical
